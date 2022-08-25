dbe0613f37f917a369322daef6a4f142161d782f
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-08-25 22:18:55 +0000

    checkout complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import { Container, Button, Nav, Navbar as NavbarBs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 export function Navbar() {
+  const { openCart, cartQuantity } = useShoppingCart();
   return (
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
@@ -19,7 +21,8 @@ export function Navbar() {
           </Nav.Link>
         </Nav>
 
-        <Button
+       {cartQuantity > 0 && ( <Button
+        onClick={openCart}
           style={{ width: "3rem", height: "3rem", position: "relative" }}
           variant="outline-primary"
           className="rounded-circle"
@@ -36,9 +39,9 @@ export function Navbar() {
           </svg>
           <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{ color: "white", width: "1.5rem", height: "1.5rem", 
           position: "absolute", bottom: 0, right: 0, transform: "translate(25%, 25%"}}>
-            3
+            {cartQuantity}
           </div>
-        </Button>
+        </Button>)}
       </Container>
     </NavbarBs>
   );
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,14 +1,20 @@
 import { useContext, useState, createContext, ReactNode } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type ShoppingCartProviderProps = {
     children: ReactNode
 }
 
 type ShoppingCartContext = {
+    openCart: () => void
+    closeCart: () => void
     getItemQuantity: (id: number) => number
     increaseCartQuantity: (id: number) => void
     decreaseCartQuantity: (id: number) => void
     removeFromCart: (id: number) => void
+    cartQuantity: number
+    cartItems: CartItem[]
 }
 
 type CartItem = {
@@ -23,7 +29,8 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
-    const [cartItems, setCartItems] = useState<CartItem[]>([])
+    const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart", [])
+    const [isOpen, setIsOpen] = useState(false)
 
 
     function getItemQuantity(id: number) {
@@ -72,11 +79,13 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
         })
     }
 
-
-
-    return (
-    <ShoppingCartContext.Provider value={{getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart}}>
+    const cartQuantity = cartItems.reduce((quantity, item) => item.quantity + quantity, 0)
+    const openCart = () => setIsOpen(true);
+    const closeCart = () => setIsOpen(false)
+     return (
+    <ShoppingCartContext.Provider value={{getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart, cartItems, cartQuantity, openCart, closeCart}}>
         {children}
+        <ShoppingCart isOpen={isOpen} />
     </ShoppingCartContext.Provider>
     )
 }
